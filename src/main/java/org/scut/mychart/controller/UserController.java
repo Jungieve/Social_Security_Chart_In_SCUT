c16abd6fb9dcd3177b16496078718cd425b15760
Jungieve <zihan.cong@gmail.com>
2016-07-04 17:03:47 +0800

    修改controller以便于测试

--- src/main/java/org/scut/mychart/controller/UserController.java
+++ src/main/java/org/scut/mychart/controller/UserController.java
@@ -8,15 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;  
   
 import org.scut.mychart.model.AC01;  
-import org.scut.mychart.service.IUserService;  
-  
+import org.scut.mychart.service.IUserService;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller  
 @RequestMapping("/user")  
 public class UserController {  
     @Resource  
     private IUserService userService;  
       
-    @RequestMapping("/showUser")  
+    @RequestMapping("/showUser")
+    @ResponseBody
     public String toIndex(HttpServletRequest request,Model model){  
         String userId = String.valueOf(request.getParameter("id"));  
         AC01 ac01 = this.userService.getAC01ById(userId);  
