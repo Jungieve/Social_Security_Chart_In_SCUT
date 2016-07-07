fa0f719640b2dee935417f408551c2c1af6b4f8b
Season <Season@DESKTOP-D9RADST>
2016-07-07 17:22:08 +0800

    创建测试的controller。并测试仓库上传功能。

---
+++ src/main/java/org/scut/mychart/controller/Chart01Controller.java
@@ -0,0 +1,32 @@
+package org.scut.mychart.controller;
+
+import javax.annotation.Resource;
+import javax.jws.WebResult;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+  
+import org.scut.mychart.model.AC01;  
+import org.scut.mychart.service.IUserService;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.abel533.echarts.Option;
+
+@Controller  
+@RequestMapping("/charts")  
+public class Chart01Controller {  
+    @Resource  
+    private IUserService userService;  
+      
+    @RequestMapping("/chart01")
+    @ResponseBody
+    public String toIndex(HttpServletRequest request,Model model){
+
+    	Option option = this.userService.getChart01Option("endowment");
+    	model.addAttribute("data", option); 
+        return "showChart01";  //view?
+
+    }  
+}  
