1c5a13ec27f4af2ebb7612864f8f8ec46912bca0
Xuh723 <xuh723@163.com>
2016-07-19 10:53:51 +0800

    添加了雷达图的内容

---
+++ src/main/java/org/scut/mychart/mapper/ChartRadarMapper.java
@@ -0,0 +1,11 @@
+package org.scut.mychart.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.scut.mychart.model.*;
+
+public interface ChartRadarMapper {
+	List<ChartRadar> selectChartRadar();
+}	
+
---
+++ src/main/java/org/scut/mychart/service/ChartRadarService.java
@@ -0,0 +1,15 @@
+package org.scut.mychart.service;
+
+import java.util.List;
+
+import com.github.abel533.echarts.json.GsonOption;
+import org.scut.mychart.model.*;
+
+import com.github.abel533.echarts.Option;
+
+public interface ChartRadarService {
+
+    public List<ChartRadar> getChartRadarData();
+    public Integer getTypeint(String type);
+    public GsonOption getChartRadarOption();
+}
