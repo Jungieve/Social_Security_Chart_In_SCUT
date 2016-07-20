cc2ce01e46402870e5db75b2b6660439760b1223
GuiaLiang <767245872@qq.com>
2016-07-20 17:32:41 +0800

    构造了假数据

--- src/main/webapp/WEB-INF/public/js/data.js
+++ src/main/webapp/WEB-INF/public/js/data.js
@@ -721,5 +721,285 @@ var chartsOption = {
 						} ]
 					} ]
 				} ]
+	},
+	
+	theme3 : {
+		options : [
+				{
+					title : {
+						text : '未来一周气温变化',
+						subtext : '纯属虚构'
+					},
+					tooltip : {
+						trigger : 'axis'
+					},
+					legend : {
+						data : [ '最高气温', '最低气温' ]
+					},
+					toolbox : {
+						show : false,
+						feature : {
+							mark : {
+								show : true
+							},
+							dataView : {
+								show : true,
+								readOnly : false
+							},
+							magicType : {
+								show : true,
+								type : [ 'line', 'bar' ]
+							},
+							restore : {
+								show : true
+							},
+							saveAsImage : {
+								show : true
+							}
+						}
+					},
+					calculable : true,
+					xAxis : [ {
+						type : 'category',
+						boundaryGap : false,
+						data : [ '周一', '周二', '周三', '周四', '周五', '周六', '周日' ]
+					} ],
+					yAxis : [ {
+						type : 'value',
+						axisLabel : {
+							formatter : '{value} °C'
+						}
+					} ],
+					series : [ {
+						name : '最高气温',
+						type : 'line',
+						data : [ 11, 11, 15, 13, 12, 13, 10 ],
+						markPoint : {
+							data : [ {
+								type : 'max',
+								name : '最大值'
+							}, {
+								type : 'min',
+								name : '最小值'
+							} ]
+						},
+						markLine : {
+							data : [ {
+								type : 'average',
+								name : '平均值'
+							} ]
+						}
+					} ]
+				} ]
+	},
+	
+	theme4 : {
+		options : [
+				{
+					title : {
+						text : '未来一周气温变化',
+						subtext : '纯属虚构'
+					},
+					tooltip : {
+						trigger : 'axis'
+					},
+					legend : {
+						data : [ '最高气温', '最低气温' ]
+					},
+					toolbox : {
+						show : false,
+						feature : {
+							mark : {
+								show : true
+							},
+							dataView : {
+								show : true,
+								readOnly : false
+							},
+							magicType : {
+								show : true,
+								type : [ 'line', 'bar' ]
+							},
+							restore : {
+								show : true
+							},
+							saveAsImage : {
+								show : true
+							}
+						}
+					},
+					calculable : true,
+					xAxis : [ {
+						type : 'category',
+						boundaryGap : false,
+						data : [ '周一', '周二', '周三', '周四', '周五', '周六', '周日' ]
+					} ],
+					yAxis : [ {
+						type : 'value',
+						axisLabel : {
+							formatter : '{value} °C'
+						}
+					} ],
+					series : [ {
+						name : '最高气温',
+						type : 'line',
+						data : [ 11, 11, 15, 13, 12, 13, 10 ],
+						markPoint : {
+							data : [ {
+								type : 'max',
+								name : '最大值'
+							}, {
+								type : 'min',
+								name : '最小值'
+							} ]
+						},
+						markLine : {
+							data : [ {
+								type : 'average',
+								name : '平均值'
+							} ]
+						}
+					} ]
+				} ]
+	},
+	
+	theme5 : {
+		options : [
+				{
+					title : {
+						text : '未来一周气温变化',
+						subtext : '纯属虚构'
+					},
+					tooltip : {
+						trigger : 'axis'
+					},
+					legend : {
+						data : [ '最高气温', '最低气温' ]
+					},
+					toolbox : {
+						show : false,
+						feature : {
+							mark : {
+								show : true
+							},
+							dataView : {
+								show : true,
+								readOnly : false
+							},
+							magicType : {
+								show : true,
+								type : [ 'line', 'bar' ]
+							},
+							restore : {
+								show : true
+							},
+							saveAsImage : {
+								show : true
+							}
+						}
+					},
+					calculable : true,
+					xAxis : [ {
+						type : 'category',
+						boundaryGap : false,
+						data : [ '周一', '周二', '周三', '周四', '周五', '周六', '周日' ]
+					} ],
+					yAxis : [ {
+						type : 'value',
+						axisLabel : {
+							formatter : '{value} °C'
+						}
+					} ],
+					series : [ {
+						name : '最高气温',
+						type : 'line',
+						data : [ 11, 11, 15, 13, 12, 13, 10 ],
+						markPoint : {
+							data : [ {
+								type : 'max',
+								name : '最大值'
+							}, {
+								type : 'min',
+								name : '最小值'
+							} ]
+						},
+						markLine : {
+							data : [ {
+								type : 'average',
+								name : '平均值'
+							} ]
+						}
+					} ]
+				} ]
+	},
+	
+	theme6 : {
+		options : [
+				{
+					title : {
+						text : '未来一周气温变化',
+						subtext : '纯属虚构'
+					},
+					tooltip : {
+						trigger : 'axis'
+					},
+					legend : {
+						data : [ '最高气温', '最低气温' ]
+					},
+					toolbox : {
+						show : false,
+						feature : {
+							mark : {
+								show : true
+							},
+							dataView : {
+								show : true,
+								readOnly : false
+							},
+							magicType : {
+								show : true,
+								type : [ 'line', 'bar' ]
+							},
+							restore : {
+								show : true
+							},
+							saveAsImage : {
+								show : true
+							}
+						}
+					},
+					calculable : true,
+					xAxis : [ {
+						type : 'category',
+						boundaryGap : false,
+						data : [ '周一', '周二', '周三', '周四', '周五', '周六', '周日' ]
+					} ],
+					yAxis : [ {
+						type : 'value',
+						axisLabel : {
+							formatter : '{value} °C'
+						}
+					} ],
+					series : [ {
+						name : '最高气温',
+						type : 'line',
+						data : [ 11, 11, 15, 13, 12, 13, 10 ],
+						markPoint : {
+							data : [ {
+								type : 'max',
+								name : '最大值'
+							}, {
+								type : 'min',
+								name : '最小值'
+							} ]
+						},
+						markLine : {
+							data : [ {
+								type : 'average',
+								name : '平均值'
+							} ]
+						}
+					} ]
+				} ]
 	}
 };
