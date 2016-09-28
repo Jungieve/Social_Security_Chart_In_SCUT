3902f4644e03e19ef3093719abe86e2a98f3d49e
GuiaLiang <767245872@qq.com>
2016-09-28 17:18:27 +0800

    提交相关的推荐页面展示

--- src/main/java/org/scut/mychart/controller/RecommendController.java
+++ src/main/java/org/scut/mychart/controller/RecommendController.java
@@ -1,18 +1,15 @@
 package org.scut.mychart.controller;
 
-import org.scut.mychart.model.MapChartModel;
 import org.scut.mychart.model.RecommendResultModel;
-import org.scut.mychart.service.MapChartService;
 import org.scut.mychart.service.impl.RecommendServiceImpl;
-import org.scut.mychart.util.DictionaryString;
+import org.scut.mychart.util.JSONUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +18,7 @@ import java.util.Map;
  * @author gzw
  */
 @Controller
-@RequestMapping(value = "/recommend",produces = "text/html;charset=UTF-8")
+@RequestMapping(value = "/recommend")
 public class RecommendController {
     @Resource
     private RecommendServiceImpl recommendService;
@@ -31,13 +28,12 @@ public class RecommendController {
      * @param id
      * @return
      */
-    @RequestMapping(value = "/result",produces = "application/json")
+    @RequestMapping(value = "/result",produces = "application/json;charset=UTF-8")
     @ResponseBody
     public Map<String, Object> getChartMap3dMarkBarOption(@RequestParam("id") String id){
         Map<String,Object> result=new HashMap<String,Object>();
         List<RecommendResultModel> recommendResultModelList =recommendService.getRecommendResult(id);
         result.put("data",recommendResultModelList);
-        System.out.println(result);
         return result;
     }
 }   
